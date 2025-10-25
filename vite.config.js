cce4a2a51b33fa4478adb4d7aeed6843245f61ec
ezekieltemitope <alebiosutemitope79@gmail.com>
2025-10-25 19:55:46 +0100

    changes made in config page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MarkAfe-Elevated-Technologies/', // <-- your repo name
   plugins: [
     react(),
     tailwindcss(),

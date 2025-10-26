11b0e3f140d8add987c1a9813bc2906ecbbf5ce1
ezekieltemitope <alebiosutemitope79@gmail.com>
2025-10-26 13:05:31 +0100

    bug corrected

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MarkAfe-Elevated-Technologies/', // <-- your repo name
   plugins: [
     react(),
     tailwindcss(),
